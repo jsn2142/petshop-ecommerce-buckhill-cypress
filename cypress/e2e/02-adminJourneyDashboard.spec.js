340fa2d08a55d7f61daf265495057307c5b1ecd9
jsn2142 <jsn2142@gmail.com>
2023-05-09 01:51:55 +0600

    Updated spec 02 🚀

--- cypress/e2e/02-adminJourneyDashboard.spec.js
+++ cypress/e2e/02-adminJourneyDashboard.spec.js
@@ -1,7 +1,7 @@
 // --- Created by Jesan Rahman ---
 
 describe('Admin journey: Shipping Information', () => {
-    beforeEach('Admin Login', () => {
+    it('Admin Login', () => {
         cy.visitAdminLoginPage()
         cy.fixture('data').then((data) => {
             var email = data.admin.email
@@ -12,14 +12,4 @@ describe('Admin journey: Shipping Information', () => {
     it('Check dashboard elements', () => {
         cy.checkDashboardElements()
     })
-    // it('Check Shipment location with different filters', () => {
-    //     cy.checkShippingInfo()
-    // })
-    // it('Check and Add Customer information', () => {
-    //     cy.checkUserInfo()
-    // })
-    // it('Search a product', () => {
-    //     cy.searchProduct()
-    // })
-
 })
